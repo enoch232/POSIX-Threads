ab7dc8f7cff966adfe657934310c82ebe32988ec
Enoch Ko <enoch232@gmail.com>
2017-02-28 23:16:16 -0700

    make the timing right, stop producers from starting over agagin

--- array-sum.c
+++ array-sum.c
@@ -15,6 +15,7 @@ typedef struct {
     int c[ARRAY_SIZE];
     int array_index;
     int global_sum;
+    int counter;
     sem_t count;           /* keep track of the number of full spots */
     pthread_mutex_t mutex;          /* enforce mutual exclusion to shared data */
 } sem_var_t;
@@ -25,27 +26,31 @@ sem_var_t shared_variables;
 void *Producer(void *arg) {
     int index = (int) arg;
     while (shared_variables.array_index < 20){
+
       sem_wait(&shared_variables.count);
       pthread_mutex_lock(&shared_variables.mutex);
       shared_variables.c[shared_variables.array_index] = shared_variables.a[shared_variables.array_index] + shared_variables.b[shared_variables.array_index];
       printf("Producer %d has written c[%d] and updated global sum.\n", index, shared_variables.array_index);
+      // sleep(1);
       shared_variables.array_index += 1;
       pthread_mutex_unlock(&shared_variables.mutex);
+      // shared_variables.counter ++;
       sem_post(&shared_variables.count);
-      // sleep(1);
+      sleep(1);
     }
+
     return NULL;
 }
 
 void *Consumer(void *arg) {
     int index = (int) arg;
     for (int i = 0; i < 20; i++) {
+      sleep(1);
       sem_wait(&shared_variables.count);
       pthread_mutex_lock(&shared_variables.mutex);
       printf("Consumer %d has read c[%d]= %d.\n", index, i, shared_variables.c[i]);
+      // shared_variables.counter --;
       pthread_mutex_unlock(&shared_variables.mutex);
-      // printf("Consumer created! %d\n", shared_variables.c[i]);
-
     }
     return NULL;
 }
@@ -54,7 +59,6 @@ int main() {
     pthread_t idP, idC;
     int index;
     sem_init(&shared_variables.count, 0, 0);
-
     for(int i = 0; i < 20; i++) {
       shared_variables.a[i] = i;
     }
@@ -66,7 +70,8 @@ int main() {
     }
     shared_variables.array_index = 0;
     shared_variables.global_sum = 0;
-
+    shared_variables.count = 0;
+    pthread_mutex_init(&shared_variables.mutex, NULL);
     for (index = 0; index < PRODUCER_COUNT; index++) {
         /* Create a new producer */
         pthread_create(&idP, NULL, Producer, (void*)index);
